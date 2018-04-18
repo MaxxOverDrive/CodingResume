bfe5a894ae329e308e7448fdc2c3edc2529d1f1b
MaxxOverDrive <codepro21@gmail.com>
2018-04-17 20:35:26 -0700

    Resume

---
+++ index.php
@@ -0,0 +1,273 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Matt's Resume</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<meta name="B-verify" content="888ae82e667b8116b02c72860cda3440731ce241" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+  <style>
+    .section_title {
+      font-weight: 800;
+      text-align: center;
+      font-family: serif;
+    }
+    .section_area {
+      border: 1px solid black;
+      font-size: 125%;
+      font-family: serif;
+      padding: 1%;
+    }
+		.job_title {
+			font-size: 110%;
+			font-weight: 600;
+		}
+		.job_desc {
+			font-family: cursive;
+		}
+
+    .skills_title {
+      font-weight: 600;
+      text-align: center;
+    }
+    .skills_list {
+
+    }
+
+    .ref_name {
+      padding: 2%;
+      font-family: serif;
+    }
+    .ref_type_header {
+      font-family: cursive;
+      font-weight: 600;
+    }
+    .ref_type {
+      padding: 2%;
+      font-family:
+    }
+    .ref_contact {
+      padding: 2%;
+      font-family: serif;
+    }
+		#education_table {
+			width: 80%;
+			margin: auto;
+			font-size: 125%;
+			font-family: cursive;
+		}
+    #skills_section {
+			font-size: 125%;
+			font-family: cursive;
+    }
+  </style>
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12">
+        <div class="col-sm-9">
+          <h3 class="section_title">Summary</h3>
+          <div class="section_area">
+            <p>Experienced Web Developer very eager to continue learning is seeking a
+               position that both challenges and improves my technical abilities. Has a passion
+               for automating tasks and improving workflow and strives to be a professional
+               in the Network Security field.</p>
+          </div>
+        </div>
+        <div class="col-sm-3">
+          <h3 class="section_title">Info</h3>
+          <table>
+            <tr>
+              <td><img src="Img/InfoIconGraphic1.png" alt="Graphic Not Displayed!" /></td>
+              <td>Matt Williams</td>
+            </tr>
+            <tr>
+              <td><img src="Img/PhoneIcone.png" alt="Graphic Not Displayed!" /></td>
+              <td></td>
+            </tr>
+            <tr>
+              <td><img src="Img/EmailIconGraphic.png" alt="Graphic Not Displayed!" /></td>
+              <td>codepro21@gmail.com</td>
+            </tr>
+            <tr>
+              <td><img src="Img/LinkedinIcon.png" alt="Graphic Not Displayed!" /></td>
+              <td></td>
+            </tr>
+          </table>
+        </div>
+      </div>
+      <div class="col-sm-12">
+        <h3 class="section_title">Experience</h3>
+				<div class="col-sm-4">
+					<p class="job_title">Web Developer<br />
+					Material Flow & Conveyors</p>
+				</div>
+				<div class="col-sm-3 col-sm-offset-5">
+					<p class="job_title">02/2017 to Present<br />
+					Donald, OR.</p>
+				</div>
+					<div class="col-sm-10 col-sm-offset-1">
+						<p class="job_desc">Scrape and filter data from websites and Excel sheets using PHP & Regular Expressions
+						to be stored in database to be dynamically displayed. Optimize load speed by
+						debugging, simplifying and minifying code. Create landing pages for Google Ad words
+						and client navigation efficiency. Perform Penetration tests on login systems via SQL Injections
+						to help ensure authorized access only. C# with ASP .NET MVC experience. See website at https://materialflow.com</p>
+					</div>
+
+					<div class="col-sm-4">
+						<p class="job_title">Web Developer<br />
+						Lattice Semiconductor</p>
+					</div>
+					<div class="col-sm-3 col-sm-offset-5">
+						<p class="job_title">03/16/2017 to 08/25/2017<br />
+						Portland, OR.</p>
+					</div>
+						<div class="col-sm-10 col-sm-offset-1">
+							<p class="job_desc">Contracted out to Lattice Semiconductor to update and provide Quality Assurance to Support Database.
+							Working with their development team both in office and remotely to implement around
+							2200 FAQ's answering client questions regarding Lattice software. Examples of Work here can be found at
+							URL http://www.latticesemi.com/en/Support/AnswerDatabase.<br />
+							Contact: Jamie Scott at Ultimate Staffing (503)684.1013</p>
+						</div>
+
+						<div class="col-sm-4">
+							<p class="job_title">Web Developer<br />
+							PEI Manufacturing</p>
+						</div>
+						<div class="col-sm-3 col-sm-offset-5">
+							<p class="job_title">04/05/2016 to 02/25/2017<br />
+							Portland, OR.</p>
+						</div>
+						<div class="col-sm-10 col-sm-offset-1">
+							<p class="job_desc">Contact: Jarrod Royce             (360)831-3308</p>
+						</div>
+			</div>
+    <div class="col-sm-12">
+      <h3 class="section_title">Education</h3>
+			<div class="col-sm-10 col-sm-offset-1 well">
+				<table id="education_table">
+					<tr>
+						<td>Truckee Meadows Community College</td>
+						<td>2012/2013</td>
+					</tr>
+					<tr>
+						<td>Reno, NV.</td>
+						<td>Fire Science Emphasis</td>
+					</tr>
+					<tr>
+						<td><br /></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Basic Underwater Demolition for Navy Seals</td>
+						<td>2/2001-9/2001</td>
+					</tr>
+					<tr>
+						   <td>Coronado, CA.</td>
+							 <td>Team Building</td>
+					</tr>
+					<tr>
+						<td><br /></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Naval Electrician School</td>
+						<td>9/2000-2/2001</td>
+					</tr>
+					<tr>
+						<td>Great Lakes, IL.</td>
+						<td>Electrical Circuit Mapping</td>
+					</tr>
+					<tr>
+						<td><br /></td>
+						<td></td>
+					</tr>
+					<tr>
+						<td>Rancho Vist High School</td>
+						<td>Graduated 06/2000</td>
+					</tr>
+						<td>Temecula/CA.</td>
+						<td>Computer Applications Emphasis</td>
+					</tr>
+				</table>
+			</div>
+    </div>
+
+    <div class="col-sm-12">
+      <h3 class="section_title">Certifications</h3>
+    </div>
+
+    <div class="col-sm-12">
+      <h3 class="section_title">Skills</h3>
+      <div id="skills_section">
+        <div class="col-sm-4">
+          <h4 class="skills_title">Programming Languages</h4>
+          <ul class="skills_list">
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>JAVASCRIPT</li>
+            <li>Jquery</li>
+            <li>PHP</li>
+            <li>MySQL</li>
+            <li>PYTHON</li>
+            <li>LINUX</li>
+          </ul>
+        </div>
+        <div class="col-sm-4">
+          <h4 class="skills_title">Frameworks</h4>
+          <ul class="skills_list">
+            <li>bootstrap</li>
+            <li>PHP Unit</li>
+            <li>ASP.NET MVC</li>
+          </ul>
+        </div>
+        <div class="col-sm-4">
+          <h4 class="skills_title">Additional</h4>
+          <ul class="skills_list">
+            <li>Adobe Photoshop</li>
+            <li>Adobe Illustrator</li>
+            <li>Visual Studio</li>
+            <li>Git</li>
+            <li>COMPOSER</li>
+            <li>Virtualbox</li>
+            <li>Google Analytics</li>
+            <li>Google Ad Words</li>
+          </ul>
+      </div>
+    </div>
+  </div>
+
+    <div class="col-sm-12 text-center">
+      <h3 class="section_title">REFERENCES:</h3>
+      <table style="margin: auto; width: 80%; font-size: 125%;">
+        <tr style="font-weight: 600; font-size: 125%;">
+          <td>Name</td>
+          <td>Reference Type</td>
+          <td>Contact</td>
+        </tr>
+        <tr>
+          <td class="ref_name">Adam Lindsey</td>
+          <td class="ref_type"><span class="ref_type_header">Current Manager</span><br /> Material Flow & Conveyor Systems</td>
+          <td class="ref_contact">(541)281-0041</td>
+        </tr>
+        <tr>
+          <td class="ref_name">Jarrod Royce</td>
+          <td class="ref_type"><span class="ref_type_header">Previous Manager</span><br /> PEI MFG</td>
+          <td class="ref_contact">(360)831-3308</td>
+        </tr>
+        <tr>
+          <td class="ref_name">Sam Pedraza</td>
+          <td class="ref_type"><span class="ref_type_header">Project Manager</span><br /> Iterate Marketing</td>
+          <td class="ref_contact">(503)560-6162<br />
+              samdpedraza@gmail.com</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+</div>
+<!--Javascript Links-->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</body>
+</html>
